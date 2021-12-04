0506ac13d952eb3550ed2d3b743a58289a1da6a4
Martin Bavio <mbavio@gmail.com>
2021-12-04 01:59:08 -0300

    Complete MVP

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,63 +1,20 @@
-import type { MetaFunction, LoaderFunction } from "remix";
-import { useLoaderData, json, Link } from "remix";
+import * as React from "react";
+import type { MetaFunction } from "remix";
 
-type IndexData = {
-  resources: Array<{ name: string; url: string }>;
-  demos: Array<{ name: string; to: string }>;
-};
-
-// Loaders provide data to components and are only ever called on the server, so
-// you can connect to a database or run any server side code you want right next
-// to the component that renders it.
-// https://remix.run/api/conventions#loader
-export let loader: LoaderFunction = () => {
-  let data: IndexData = {
-    resources: [
-      {
-        name: "Remix Docs",
-        url: "https://remix.run/docs",
-      },
-      {
-        name: "React Router Docs",
-        url: "https://reactrouter.com/docs",
-      },
-      {
-        name: "Remix Discord",
-        url: "https://discord.gg/VBePs6d",
-      },
-    ],
-    demos: [
-      {
-        to: "demos/actions",
-        name: "Actions",
-      },
-      {
-        to: "demos/about",
-        name: "Nested Routes, CSS loading/unloading",
-      },
-      {
-        to: "demos/params",
-        name: "URL Params and Error Boundaries",
-      },
-    ],
-  };
-
-  // https://remix.run/api/remix#json
-  return json(data);
-};
-
-// https://remix.run/api/conventions#meta
-export let meta: MetaFunction = () => {
+export const meta: MetaFunction = () => {
   return {
-    title: "Remix Starter",
-    description: "Welcome to remix!",
+    title: "Moulton",
+    description: "A Remix Newsletter",
   };
 };
 
-// https://remix.run/guides/routing#index-routes
 export default function Index() {
-  let data = useLoaderData<IndexData>();
-
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  React.useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
   return (
     <section className="container">
       <main>
@@ -73,6 +30,7 @@ export default function Index() {
           <input
             type="email"
             placeholder="Your email to receive Remix news every Monday"
+            ref={inputRef}
           />
           <button type="submit">
             <span>Subscribe</span>
